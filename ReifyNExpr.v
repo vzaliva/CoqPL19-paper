6f25bd3d3f1b58a667c2fefb8dd8b2ea85f63da0
Vadim Zaliva <lord@crocodile.org>
2018-10-02 15:46:46 -0700

    refactoring

--- ReifyNExpr.v
+++ ReifyNExpr.v
@@ -26,12 +26,14 @@ Run TemplateProgram
     ).
 
 Section Reify.
+  Local Definition inat :=  {| inductive_mind := "Coq.Init.Datatypes.nat"; inductive_ind := 0 |}.
+  Local Definition nt := tInd inat [].
+
   Local Opaque Nat.add Nat.sub Nat.mul.
 
   Definition varlist:Type := list string.
 
   Fixpoint compileNExpr (params:varlist) (a_n:term): TemplateMonad (varlist*NExpr) :=
-    let inat :=  {| inductive_mind := "Coq.Init.Datatypes.nat"; inductive_ind := 0 |} in
     match a_n with
     | (tConstruct inat 0 []) => tmReturn (params, NConst 0)
     | (tApp (tConstruct inat 1 []) [e]) =>
@@ -52,18 +54,18 @@ Section Reify.
           | Some n_Mul => tmReturn (params, NMult d_a' d_b')
           | None => tmFail ("Unknown binary function" ++ bfun)
           end
-    | (tLambda (nNamed n) (tInd inat []) b_n) =>  compileNExpr (n::params) b_n
+    | (tLambda (nNamed n) nt b_n) =>  compileNExpr (n::params) b_n
     | tRel n => tmReturn (params, NVar n)
     | _ => tmFail ("Unsupported NExpr" ++ (string_of_term a_n))
     end.
 
   Fixpoint build_param_list (l:varlist) : TemplateMonad term :=
     match l with
-    | [] => tmReturn (tApp (tConstruct {| inductive_mind := "Coq.Init.Datatypes.list"; inductive_ind := 0 |} 0 []) [tInd {| inductive_mind := "Coq.Init.Datatypes.nat"; inductive_ind := 0 |} []])
+    | [] => tmReturn (tApp (tConstruct {| inductive_mind := "Coq.Init.Datatypes.list"; inductive_ind := 0 |} 0 []) [nt])
     | x::xs =>
       let i := length xs in
       ts <- build_param_list xs ;;
-         tmReturn (tApp (tConstruct {| inductive_mind := "Coq.Init.Datatypes.list"; inductive_ind := 0 |} 1 []) [tInd {| inductive_mind := "Coq.Init.Datatypes.nat"; inductive_ind := 0 |} []; tRel i; ts])
+         tmReturn (tApp (tConstruct {| inductive_mind := "Coq.Init.Datatypes.list"; inductive_ind := 0 |} 1 []) [nt; tRel i; ts])
     end.
 
   Fixpoint iota (from:nat) (len: nat) : list nat :=
@@ -73,11 +75,13 @@ Section Reify.
     end.
 
   Fixpoint build_forall (p:varlist) conc :=
-    let nt := tInd {| inductive_mind := "Coq.Init.Datatypes.nat"; inductive_ind := 0 |} []in
     match p with
     | [] => conc
     | n::ps => tProd (nNamed n) nt (build_forall ps conc)
     end.
+
+  Definition build_forall' (p:varlist) conc :=
+    List.fold_right (fun n ps => tProd (nNamed n) nt ps) conc p.
 
   Polymorphic Definition reifyNExp@{t u} {A:Type@{t}}
               (res_name: string)
@@ -98,7 +102,7 @@ Section Reify.
          let a_exp := tApp ast param_idx in
          a_c <- tmQuote c' ;;
              let lemma_concl := tApp (tConst "NExpr_term_equiv" []) [a_params; a_c; a_exp] in
-             let lemma_ast := build_forall params lemma_concl in
+             let lemma_ast := build_forall' params lemma_concl in
              (tmBind (tmUnquoteTyped Prop lemma_ast)
                      (fun lemma_body => tmLemma lemma_name lemma_body
                                              ;;
