4b0598516e90f888f1c19ab5bc6197e3cd17377c
Vadim Zaliva <lord@crocodile.org>
2019-01-19 13:01:12 +0000

    fixed file name

--- present.sh
+++ present.sh
@@ -4,8 +4,8 @@
 # (for `dunst` daemon on Lunux)
 notify-send DUNST_COMMAND_PAUSE
 
-pympress slides-horizontal.pdf
-#pdfpc -s -w -c --notes=bottom proposal-slides.pdf
+pympress slides.pdf
+#pdfpc -s -w -c --notes=bottom slides.pdf
 
 # re-enable desktop notifications
 notify-send DUNST_COMMAND_RESUME
